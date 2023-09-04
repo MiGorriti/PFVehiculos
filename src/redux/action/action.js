f711385c99ca3bfe5443f3f77e87e4a1b6b82efd
fabian <fasalpe34@gmail.ocm>
2023-09-04 16:01:45 -0500

    FRONT ACTUALIZADO

---
+++ src/redux/action/action.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { GET_CARS, GET_DETAIL,ORDER_BY_NAME,ORDER_BY_PRICE } from './typeAction';
+
+export const getCars = () => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/product/`
+      );
+
+   
+      const cars = response.data;
+      console.log(cars);
+
+      dispatch({ type: GET_CARS, payload: cars });
+    } catch (error) {
+      
+      console.error("Error al obtener datos de los coches:", error);
+    }
+  };
+};
+
+export const getDetail = (idCar)=>{
+    return async function(dispatch){
+        try {
+            const response = await axios.get(`http://localhost:3001/product/${idCar}`)
+            
+            const detail = response.data;
+            console.log(detail);
+            dispatch({type: GET_DETAIL, payload: detail })
+        } catch (error) {
+            console.error("Error");
+        }
+    }
+}
+
+
+// export function postVehiculo(state){
+//     return async function(dispatch){
+//         try {
+//             const response = await axios.post("http://localhost:3001/update/product", state)
+//             alert("Se publico correctamente.")
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+// }
+
+export const OrderByName = (payload)=>{
+  return{
+    type: ORDER_BY_NAME,
+    payload
+  }
+}
+
+export const OrderByPrice = (payload)=>{
+  return{
+    type: ORDER_BY_PRICE,
+    payload
+  }
+}
